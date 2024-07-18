4f2e8fccd9fc244b23c78b8c86d216b5eeab1d7e
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-07-18 11:28:49 +0200

    Code update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
 const API_KEY = '44933716-9a3a9b2f063fdc3971bf0291b';
 const BASE_URL = 'https://pixabay.com/api/';
@@ -9,16 +10,28 @@ const BASE_URL = 'https://pixabay.com/api/';
 const searchForm = document.getElementById('search-form');
 const gallery = document.getElementById('gallery');
 const loader = document.getElementById('loader');
+const loadMoreBtn = document.getElementById('load-more-btn');
+let query = '';
+let page = 1;
+let totalHits = 0;
 
-const fetchImages = query => {
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+const fetchImages = async (query, page = 1) => {
+  try {
+    const response = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: 40,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.status);
+  }
 };
 
 const createImageMarkup = images => {
@@ -43,7 +56,7 @@ const createImageMarkup = images => {
 
 const displayImages = images => {
   const markup = createImageMarkup(images);
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
   const lightbox = new SimpleLightbox('.gallery-link', {
     captionsData: 'alt',
     captionDelay: 250,
@@ -59,18 +72,28 @@ const showError = message => {
   });
 };
 
+const showInfo = message => {
+  iziToast.info({
+    title: 'Info',
+    message: message,
+  });
+};
+
 const handleSearchFormSubmit = async event => {
   event.preventDefault();
-  const query = event.target.elements.query.value.trim();
+  query = event.target.elements.query.value.trim();
 
   if (!query) return;
 
   gallery.innerHTML = '';
+  loadMoreBtn.style.display = 'none';
   loader.style.display = 'flex';
+  page = 1;
 
   try {
-    const data = await fetchImages(query);
+    const data = await fetchImages(query, page);
     const images = data.hits;
+    totalHits = data.totalHits;
 
     if (images.length === 0) {
       showError(
@@ -78,6 +101,42 @@ const handleSearchFormSubmit = async event => {
       );
     } else {
       displayImages(images);
+      if (totalHits > images.length) {
+        loadMoreBtn.style.display = 'block';
+      }
+    }
+  } catch (error) {
+    showError('Something went wrong. Please try again later.');
+  } finally {
+    loader.style.display = 'none';
+  }
+};
+
+const handleLoadMoreBtnClick = async () => {
+  page += 1;
+  loader.style.display = 'flex';
+
+  try {
+    const data = await fetchImages(query, page);
+    const images = data.hits;
+
+    if (images.length === 0) {
+      loadMoreBtn.style.display = 'none';
+      showInfo("We're sorry, but you've reached the end of search results.");
+    } else {
+      displayImages(images);
+      if (page * 40 >= totalHits) {
+        loadMoreBtn.style.display = 'none';
+        showInfo("We're sorry, but you've reached the end of search results.");
+      }
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery-item')
+        .getBoundingClientRect();
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
     }
   } catch (error) {
     showError('Something went wrong. Please try again later.');
@@ -87,3 +146,4 @@ const handleSearchFormSubmit = async event => {
 };
 
 searchForm.addEventListener('submit', handleSearchFormSubmit);
+loadMoreBtn.addEventListener('click', handleLoadMoreBtnClick);
